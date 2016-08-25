6d2abb754dfdb2103fc329a750e4e0865fdac24f
Bryan Jennings <bryanjenningz@gmail.com>
2016-08-25 12:43:24 -0700

    set up react router children components and mock data

--- src/index.js
+++ src/index.js
@@ -7,29 +7,62 @@ function App() {
 }
 
 function Nav(props) {
+  const { children } = props
   const currentPath = props.route.path
   const activePath = path => currentPath === path ? 'active' : ''
   return (
-    <nav className="navbar navbar-inverse">
-      <ul className="nav navbar-nav">
-        <li className={activePath('/') + ' col-xs-3'}><Link activeClassName="active" to="/">Home</Link></li>
-        <li className={activePath('/talks') + ' col-xs-3'}><Link activeClassName="active" to="/talks">Talks</Link></li>
-        <li className={activePath('/learn') + ' col-xs-3'}><Link activeClassName="active" to="/learn">Learn</Link></li>
-        <li className={activePath('/profile') + ' col-xs-3'}><Link activeClassName="active" to="/profile">Profile</Link></li>
-      </ul>
-    </nav>
+    <div>
+      <nav className="navbar navbar-inverse">
+        <ul className="nav navbar-nav">
+          <li className={activePath('/') + ' col-xs-3'}><Link activeClassName="active" to="/">Home</Link></li>
+          <li className={activePath('/talks') + ' col-xs-3'}><Link activeClassName="active" to="/talks">Talks</Link></li>
+          <li className={activePath('/learn') + ' col-xs-3'}><Link activeClassName="active" to="/learn">Learn</Link></li>
+          <li className={activePath('/profile') + ' col-xs-3'}><Link activeClassName="active" to="/profile">Profile</Link></li>
+        </ul>
+      </nav>
+      <div>{children}</div>
+    </div>
   )
 }
-Nav.propTypes = {
-  params: PropTypes.object.isRequired
+
+function Search() {
+  return (
+    <div>Search</div>
+  )
+}
+
+function Talks() {
+  return (
+    <div>Talks</div>
+  )
+}
+
+function Learn() {
+  return (
+    <div>Learn</div>
+  )
+}
+
+function Profile() {
+  return (
+    <div>Profile</div>
+  )
 }
 
 const routes = (
   <Router history={hashHistory}>
-    <Route path={'/'} component={Nav} />
-    <Route path={'/talks'} component={Nav} />
-    <Route path={'/learn'} component={Nav} /> 
-    <Route path={'/profile'} component={Nav} /> 
+    <Route path={'/'} component={Nav}>
+      <IndexRoute component={Search} />
+    </Route>
+    <Route path={'/talks'} component={Nav}>
+      <IndexRoute component={Talks} />
+    </Route>
+    <Route path={'/learn'} component={Nav}>
+      <IndexRoute component={Learn} />
+    </Route>
+    <Route path={'/profile'} component={Nav}>
+      <IndexRoute component={Profile} />
+    </Route>
   </Router>
 )
 
